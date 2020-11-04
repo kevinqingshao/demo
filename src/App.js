69a01ee75ccafcbb5dd82358d64ef1ec2239e8e3
kevinqingshao <kevinqingshao@gmail.com>
2020-11-03 20:40:21 -0500

    change style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import myphoto from './img/kevin.jpg'
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={myphoto}  style = {{width:"45%",height:"45%"}} alt = "my photo"/>
+        <img src={myphoto}  style = {{width:"50%",height:"50%"}} alt = "my photo"/>
         <p>
           <div>Kevin</div>
           <div>Good Luck !!!</div>
